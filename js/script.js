7ca64023b586e814c49fe05f4e4cca2893b8e682
Vyacheslav <Programmer38rus@gmail.com>
2020-05-08 16:54:13 +0800

    Немного поправил комментария для проверки синхронизации
    Signed-off-by: Vyacheslav <Programmer38rus@gmail.com>

--- js/script.js
+++ js/script.js
@@ -12,19 +12,74 @@ jQuery.prototype.click = function (fn) {
     this.each(element => element.addEventListener("click", fn));
     return this
 };
+jQuery.prototype.addReport = function (text) {
+    this.each(element => element.innerText = text);
+    return this
+};
 
-function grabValue(id) {
-    const element = document.querySelector(id);
-    return element
+function grabValue(id, timestamp) {
+    let element = document.querySelector(id).value;
+
+    if (element <= 59 && element >= 0) {
+        $("#report").addReport("");
+        element *= timestamp
+        return element;
+    } else {
+        $("#report").addReport("Вводимое вами значение должно содержать цыфры от 0 до 59");
+    }
 }
 
+const checkLength = (value, interval) => {
+    if (value.length < interval)
+        return 0 + value;
+    return value;
+};
+
+function timestampConverter(sum){
+
+    let hours = Math.floor((sum / (1000 * 60 * 60)) % 24);
+    let minutes = Math.floor((sum / (1000 * 60)) % 60);
+    let seconds = Math.floor((sum / 1000 ) % 60);
+
+    dictionary = {
+        "hours": hours,
+        "minutes": minutes,
+        "seconds": seconds,
+    };
+
+
+    return dictionary
+
+};
+
+// jQuery.prototype.add
+
 let $ = (e) => new jQuery(e);
-let hour = 0;
 
-// let a = $("#inp1");
-// console.log(grabValue("#inp1"));
-$("#btn1").click(element => hour = new grabValue("#inp1").value);
-// $("#btn1").click(a = $("#inp1").value = console.log(a));
-// $("#btn1").click(element => hour = $("#int1").value);
+let dictionary = {};
+let countDown = 0;
 
-// ПРОВЕРКА №2
+// let hour, minute, second = 0;
+// let gg = {};
+// let count = 0;
+
+$("#btn1").click(element => countDown += grabValue("#inp1", 3600000), timestampConverter(countDown));
+$("#btn2").click(element => countDown += grabValue("#inp2", 60000));
+$("#btn3").click(element => countDown += grabValue("#inp3", 1000));
+$("#start").click(element => console.log(timestampConverter(countDown)));
+$("#stop").click(element => countDown = 0);
+
+
+
+function timer() {
+
+    clock.innerText = checkLength(dictionary.hours, 2) + ":" + checkLength(dictionary.minutes, 2) + ":" + checkLength(dictionary.seconds, 2);
+    timestampConverter(countDown);
+    countDown -= 1000;
+    const time = window.setInterval(timer, 1000);
+
+};
+
+
+
+
